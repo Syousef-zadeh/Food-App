fe3a6c8d21b30f5287f84e782d49d8dd1b765433
Sheida Yousefzadeh <sheidayousefzadeh@Sheidas-MacBook-Pro.local>
2024-04-06 19:17:22 +0330

    Food add

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,8 @@
+export default function mealsSlugPage({params}){
+    return(
+        <main>
+            <h1>Hello</h1>
+            <p>{params.slug}</p>
+        </main>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
-import Header from "@/components/header";
-import NotFound from "@/components/not-found";
 
 export default function Home() {
   return (
     <main>
-      <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Share Meals</Link></p>
+      <p><Link href="/community">Community</Link></p>
     </main>
   );
 }
