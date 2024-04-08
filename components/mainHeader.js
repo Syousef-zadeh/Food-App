e8bbb8a9d44393c45e99654e240452387f32c921
Sheida Yousefzadeh <sheidayousefzadeh@Sheidas-MacBook-Pro.local>
2024-04-08 18:20:07 +0330

    main header background component

--- components/mainHeader.js
+++ components/mainHeader.js
@@ -1,23 +1,27 @@
 import logoImg from "@/assets/logo.png";
 import Link from "next/link";
 import Image from "next/image";
-import classes from './main-Header.module.css';
+import MainHeaderBackground from "./main-HeaderBackground";
+import classes from "./main-Header.module.css";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" property/>
-        Next Leve Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Brows Meals</Link>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+     <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" property />
+          Next Leve Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Brows Meals</Link>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
