c57722d6f4b1eb5132028eab5a01d53a5a7e3947
Sheida Yousefzadeh <sheidayousefzadeh@Sheidas-MacBook-Pro.local>
2024-04-15 19:09:29 +0330

    add css for meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,10 @@
-import Link from "next/link";
 
-export default function Meals(){
-    return(
-        <main>
-            <h1>Meals Page</h1>
-            <p><Link href="/meals/1">Foods</Link></p>
-            <p><Link href="/meals/2">Drinks</Link></p>
-            <p><Link href="/meals/share">Share meals</Link></p>
-        </main>
-    )
+
+export default function Meals() {
+  return (
+    <>
+    <header></header>
+      <main></main>
+    </>
+  );
 }
