9a079985001b66da5b50074e61f2708cb2fb0596
Sheida Yousefzadeh <sheidayousefzadeh@Sheidas-MacBook-Pro.local>
2024-04-15 19:18:59 +0330

    add css for meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,25 @@
-
+import Link from "next/link";
+import classes from "./page.module.css";
 
 export default function Meals() {
   return (
     <>
-    <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipes and cook it by yourselfe. It is easy and
+          fast!
+        </p>
+        <p>
+            <Link href='/meals/share'>
+                Share Your Favorite Recipe.
+            </Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
     </>
   );
 }
