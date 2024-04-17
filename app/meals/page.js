dcdcb50c94d626da3a1db52bccf0ab4ebc7ae57e
Sheida Yousefzadeh <sheidayousefzadeh@Sheidas-MacBook-Pro.local>
2024-04-17 19:41:49 +0330

    lib and meals.js

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,27 +4,27 @@ import MealsGrid from "./meals-grid";
 import { getMeals } from "@/lib/meals";
 
 export default async function Meals() {
-    const allMeals = await getMeals();
-  return (
-    <>
-      <header className={classes.header}>
-        <h1>
-          Delicious meals, created{" "}
-          <span className={classes.highlight}>by you</span>
-        </h1>
-        <p>
-          Choose your favorite recipes and cook it by yourselfe. It is easy and
-          fast!
-        </p>
-        <p className={classes.cta}>
-            <Link href='/meals/share'>
-                Share Your Favorite Recipe.
+    const meals = await getMeals();
+
+    return (
+      <>
+        <header className={classes.header}>
+          <h1>
+            Delicious meals, created{' '}
+            <span className={classes.highlight}>by you</span>
+          </h1>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is easy and fun!
+          </p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">
+              Share Your Favorite Recipe
             </Link>
-        </p>
-      </header>
-      <main className={classes.main}>
-        <MealsGrid meals={[allMeals]}/>
-      </main>
-    </>
+          </p>
+        </header>
+        <main className={classes.main}>
+          <MealsGrid meals={meals} />
+        </main>
+      </>
   );
 }
