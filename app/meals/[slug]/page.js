5535d4ecdc7ba6e84f060ab1758fe48ecb476fe9
Sheida Yousefzadeh <sheida.yousefzadeh1@gmail.com>
2024-04-22 20:03:49 +0330

    meals details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+
 export default function mealsDetailsPage({ params }) {
   return (
     <>
-      <header></header>
+      <header className={classes.header}>
+        <div className={classes.image}>
+            <Image fill/>
+        </div>
+        <div className={classes.headerText}>
+
+        </div>
+      </header>
       <main></main>
     </>
   );
