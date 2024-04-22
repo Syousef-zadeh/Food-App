6e10f5fbbd82c506fcdf8138483d1d2acc32683a
Sheida Yousefzadeh <sheida.yousefzadeh1@gmail.com>
2024-04-22 19:59:31 +0330

    meals details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,8 @@
-export default function mealsSlugPage({params}){
-    return(
-        <main>
-            <h1>Hello</h1>
-            <p>{params.slug}</p>
-        </main>
-    )
+export default function mealsDetailsPage({ params }) {
+  return (
+    <>
+      <header></header>
+      <main></main>
+    </>
+  );
 }
