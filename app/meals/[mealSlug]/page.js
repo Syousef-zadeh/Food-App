06e1085912e0dde9c32d421f51d33e66b713bcb5
Sheida Yousefzadeh <sheida.yousefzadeh1@gmail.com>
2024-04-26 10:07:21 +0330

    meals notFound error

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,28 +1,37 @@
-import Image from 'next/image';
-import { getMeal } from '@/lib/meals';
-import classes from './page.module.css';
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
 
 export default function mealsDetailsPage({ params }) {
-    const meal = getMeal(params.mealSlug);
-    meal.instructions = meal.instructions.replace(/n/, '<b />');
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/n/, "<b />");
+
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-            <Image src={meal.image} alt={meal.title} fill/>
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
-            <h1>{meal.title}</h1>
-            <p className={classes.creator}>
-                by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-            </p>
-            <p className={classes.summary}>{meal.summary}</p>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
-        <p className={classes.instructions} dangerouslySetInnerHTML={{
-            __html:meal.instructions,
-        }}></p>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
       </main>
     </>
   );
